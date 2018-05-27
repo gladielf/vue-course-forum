1040503903ed6dfbd2ed5d655e3337dc57ec042b
Unknown <gladielf@gmail.com>
2018-05-27 11:30:41 +0200

    Components, Props and Computed Properties

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,22 @@
 <template>
-  <div>
-    <div v-for="thread in threads" class="col-large push-top">
-      <h1>{{thread.title}}</h1>
-        <div class="post-list">
-          <div v-for="postId in thread.posts" class="post">
-            <div class="user-info">
-              <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
-              <a href="#">
-                <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-              </a>
-              <p class="desktop-only text-small">107 posts</p>
-            </div>
-            <div class="post-content">
-              <div>
-                {{posts[postId].text}}
-              </div>
-            </div>
-            <div class="post-date text-faded">
-              {{posts[postId].publishedAt}}
-            </div>
-          </div>
-        </div>
-    </div>
+  <div class="col-full">
+    <h1>Welcome to the forum</h1>
+    <ThreadList :threads="threads"/>
   </div>
 </template>
 
 <script>
 import sourdeData from '@/data'
+import ThreadList from './ThreadList'
 console.log(sourdeData)
 export default {
   name: 'HelloWorld',
+  components: {
+    ThreadList
+  },
   data () {
     return {
-      threads: sourdeData.threads,
+      threads: Object.values(sourdeData.threads),
       posts: sourdeData.posts,
       users: sourdeData.users
     }
---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="thread">
+    <div>
+      <p> <a href="#">{{thread.title}}</a> </p>
+      <p class="text-faded text-xsmall">
+        By <a href="">{{user.name}}</a>, {{thread.publishedAt}}.
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count">
+        {{repliesCount}} replies
+      </p>
+
+      <!-- <img src="" alt="" class="avatar-medium"> -->
+
+      <!-- <div>
+        <p class="text-xsmall">
+          <a href="">Bruce Wayne</a>
+        </p>
+        <p class="text-xsmall text-faded">2 hours ago</p>
+      </div> -->
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import sourdeData from '@/data'
+  export default {
+    props: {
+      thread: {
+        required: true,
+        type: Object
+      }
+    },
+    computed: {
+      repliesCount () {
+        return Object.keys(this.thread.posts).length - 1
+      },
+      user () {
+        return sourdeData.users[this.thread.userId]
+      }
+    }
+  }
+</script>
