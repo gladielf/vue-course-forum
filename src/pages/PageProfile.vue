d641a3f2e3327411c68c1ffbeaee525341f49931
gladielf <gladielf@gmail.com>
2019-03-17 20:04:43 +0100

    Shared Page Components

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex-grid">
 
-    <!-- <UserProfileCard
+    <UserProfileCard
+      v-if="!edit"
       :user="user"
       :userPostsCount="userPostsCount"
       :userThreadsCount="userThreadsCount"
-    /> -->
+    />
     <UserProfileCardEditor
+      v-else
       :user="user"
       :userPostsCount="userPostsCount"
       :userThreadsCount="userThreadsCount"
@@ -43,6 +45,14 @@
       UserProfileCard,
       UserProfileCardEditor
     },
+
+    props: {
+      edit: {
+        type: Boolean,
+        default: false
+      }
+    },
+
     computed: {
       ...mapGetters({
         user: 'authUser'
